1c4bea560c670ce0e5ae44ba3684dcce1e5dce00
remdeveloper15 <helamanfranco@hotmail.com>
2023-09-13 01:10:50 -0500

    Assigment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Please enter your grade: ");
+string grade = Console.ReadLine();
+int number = int.Parse(grade);
+
+if (number >= 90)
+{
+    Console.WriteLine("Your grade in letters is A");
+}
+
+else if (number >= 80)
+{
+    Console.WriteLine("Your grade in letters is B");
+}
+
+else if (number >= 70)
+{
+    Console.WriteLine("Your grade in letters is C");
+}
+
+else if (number >= 60)
+{
+    Console.WriteLine("Your grade in letters is D");
+}
+
+else
+{
+    Console.WriteLine("Your grade in letters is F");
+}
+
+if (number >= 70)
+{
+    Console.WriteLine("Congratulations! You have passed! :D");
+}
+
+else
+{
+    Console.WriteLine("Sorry! You didn't get it D;");
+}
     }
 }
